025887be12f623bb07b239a6134343004d716544
giahuyanhduy <giahuyanhduy@gmail.com>
2024-08-23 07:29:45 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Cập nhật hệ thống và cài đặt Docker (không cần sudo)
-
 apt-get install -y docker.io
 
 # Bật Docker và cho phép Docker khởi động cùng hệ thống
 systemctl start docker
 systemctl enable docker
 
- docker stop watchtower;  docker rm watchtower;  docker rmi containrrr/watchtower; \
- docker stop psclient;  docker rm psclient;  docker rmi packetstream/psclient; \
- docker run -d --restart=always -e CID=6b2H \
---name psclient packetstream/psclient:latest &&  docker run -d --restart=always \
---name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower \
+# Kiểm tra xem có container nào đang chạy không, nếu có thì dừng và xóa tất cả
+if [ "$(docker ps -q)" ]; then
+    echo "Stopping and removing all running containers..."
+    docker stop $(docker ps -q)
+    docker rm $(docker ps -aq)
+fi
+
+# Dừng, xóa và remove image cho watchtower và psclient
+docker stop watchtower
+docker rm watchtower
+docker rmi containrrr/watchtower
+
+docker stop psclient
+docker rm psclient
+docker rmi packetstream/psclient
+
+# Khởi động lại container psclient và watchtower
+docker run -d --restart=always -e CID=6b2H --name psclient packetstream/psclient:latest
+
+docker run -d --restart=always --name watchtower \
+-v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower \
 --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 psclient
-docker stop recursing_carson
-docker stop distracted_faraday
-docker rm recursing_carson
-docker rm distracted_faraday
