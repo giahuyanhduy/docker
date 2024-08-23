048eb13e0c787559404b34ca589733a7fff0e86b
giahuyanhduy <giahuyanhduy@gmail.com>
2024-08-23 09:48:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#change 1.3
-# Cập nhật hệ thống và cài đặt Docker (không cần sudo)
+#change 1.4
+# Cập nhật hệ thống và cài đặt Docker
+apt-get update
 apt-get install -y docker.io
 
 # Bật Docker và cho phép Docker khởi động cùng hệ thống
@@ -14,19 +15,18 @@ if [ "$(docker ps -q)" ]; then
     docker rm $(docker ps -aq)
 fi
 
-# Dừng, xóa và remove image cho watchtower và psclient nếu có
-docker stop watchtower || true
-docker rm watchtower || true
-docker rmi containrrr/watchtower || true
+# Xóa container và image không cần thiết
+docker stop myst || true
+docker rm myst || true
+docker rmi mysteriumnetwork/myst || true
 
-docker stop psclient || true
-docker rm psclient || true
-docker rmi packetstream/psclient || true
+# Tải về Mysterium Node Docker image
+docker pull mysteriumnetwork/myst:latest
 
-# Cài đặt và khởi động Mysterium Node
-docker pull mysterium/node:latest
-docker run -d --restart=always --cap-add NET_ADMIN --net host --name mysterium-node \
--v mysterium_node_data:/var/lib/mysterium-node mysterium/node
+# Khởi động Mysterium Node
+docker run --cap-add NET_ADMIN -d -p 4449:4449 --name myst \
+-v /home/client-repo/docker:/var/lib/mysterium-node --restart unless-stopped \
+mysteriumnetwork/myst:latest service --agreed-terms-and-conditions
 
 # Liên kết node với tài khoản Mysterium bằng API key
-docker exec mysterium-node mysterium-cli --api-key VoZzWV5bQLk0KuvJGxQotg2RBI1CyreLm21FrQBa claim
+docker exec myst myst cli mmn VoZzWV5bQLk0KuvJGxQotg2RBI1CyreLm21FrQBa
