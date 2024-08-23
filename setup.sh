0f80a274acb71c903093a5d26b592fef41bdddd6
giahuyanhduy <giahuyanhduy@gmail.com>
2024-08-23 07:02:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,9 @@ apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 
+ docker stop watchtower;  docker rm watchtower;  docker rmi containrrr/watchtower; \
+ docker stop psclient;  docker rm psclient;  docker rmi packetstream/psclient; \
+ docker run -d --restart=always -e CID=6b2H \
+--name psclient packetstream/psclient:latest &&  docker run -d --restart=always \
+--name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower \
+--cleanup --include-stopped --include-restarting --revive-stopped --interval 60 psclient
